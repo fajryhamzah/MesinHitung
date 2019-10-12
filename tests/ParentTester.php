89eb4e0a8b12e6fb142fe852509c43ebf9efd93a
fajryhamzah <fajryhamzah@gmail.com>
2019-10-12 07:47:25 +0700

    Add parent class for all test

---
+++ tests/ParentTester.php
@@ -0,0 +1,44 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Illuminate\Console\Application;
+use Illuminate\Container\Container;
+use Illuminate\Events\Dispatcher;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class ParentTester extends TestCase
+{
+    protected $commandTester;
+    protected $command;
+
+    protected function build($obj, $command)
+    {
+        $container = new Container();
+        $dispatcher = new Dispatcher();
+        $application = new Application($container, $dispatcher, '0.1');
+        $application->setName('Calculator');
+        $application->add(new $obj);
+        $this->command = $application->find($command);
+        $this->commandTester = new CommandTester($this->command);   
+    }
+
+    protected function getCommandName(): string
+    {   
+        return $this->command->getName();
+    }
+
+    public function rawCommand(array $cmd){
+        $command = array(
+            'command' => $this->getCommandName()
+        );
+        $command = array_merge($command,$cmd);
+        
+        $this->commandTester->execute($command);
+
+        return str_replace("\n","",$this->commandTester->getDisplay());
+    }
+
+
+
+}
