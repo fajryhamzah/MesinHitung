c8463ce8b1610b8f63fada4ea6ea75da090db811
fajryhamzah <fajryhamzah@gmail.com>
2019-10-12 09:32:04 +0700

    Adding pow command test

---
+++ tests/Commands/PowCommandTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests;
+
+use Jakmall\Recruitment\Calculator\Commands\PowCommand;
+use Tests\ParentTester;
+
+final class PowCommandTest extends ParentTester
+{
+
+    protected function setUp()
+    {
+        ParentTester::build(PowCommand::class,"pow");
+    }
+
+    public function testTwoSameNumber(): void
+    {   
+        $this->assertEquals('1 ^ 1 = 1',$this->rawCommand(array('base' => 1,'exp'=>1)));       
+        $this->assertEquals('2 ^ 2 = 4',$this->rawCommand(array('base' => 2,'exp'=>2)));       
+    }
+
+    public function testTwoSamedifferentNumber(): void
+    {   
+        $this->assertEquals('3 ^ 5 = 243',$this->rawCommand(array('base' => 3,'exp'=>5)));       
+        $this->assertEquals('2 ^ 10 = 1024',$this->rawCommand(array('base' => 2,'exp'=>10)));       
+    }
+
+    public function testNegativeNumber(): void
+    {   
+        $this->assertEquals('3 ^ -5 = 0.004',$this->rawCommand(array('base' => 3,'exp'=>-5)));       
+        $this->assertEquals('-2 ^ 10 = 1024',$this->rawCommand(array('base' => -2,'exp'=>10)));       
+    }
+
+    public function testEmptyNumber(): void
+    {   
+        $this->assertEquals('Argument needed',$this->rawCommand(array('base','exp')));
+    }
+
+}
